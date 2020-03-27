2576c4569a4f04d670f5b9745857c516f69689ed
moorejr5 <james.r.moore@vanderbilt.edu>
2020-03-27 13:22:04 -0500

    Trying to fix data issue.

--- StudyCounterbalancingExternalModule.php
+++ StudyCounterbalancingExternalModule.php
@@ -129,7 +129,7 @@ class StudyCounterbalancingExternalModule extends AbstractExternalModule
             $viewAsSurveyIndex = array_keys($randomizedForms, $nextInstrument)[0];
             if (!empty($randomizedForms) && !empty($recordRandom[$index]) && $nextInstrument != "") {
                 $recordData = \Records::getData($project_id, 'array', array($record));
-                if ($Proj->forms[$nextInstrument]['survey_id'] != "" && $viewSurveys[$viewAsSurveyIndex] == "yes" && ($recordData[$record][$event_id][$instrument . '_complete'] == "2" || $recordData[$record]['repeat_instances'][$event_id][$instrument][$repeat_instance][$instrument . '_complete'] == "2" || $recordData[$record]['repeat_instances'][$event_id][][$repeat_instance][$instrument . '_complete'])) {
+                if ($Proj->forms[$nextInstrument]['survey_id'] != "" && $viewSurveys[$viewAsSurveyIndex] == "yes" && ($recordData[$record][$event_id][$instrument . '_complete'] == "2" || $recordData[$record]['repeat_instances'][$event_id][$instrument][$repeat_instance][$instrument . '_complete'] == "2" || $recordData[$record]['repeat_instances'][$event_id][''][$repeat_instance][$instrument . '_complete'])) {
                     if ($recordData[$record][$event_id][$nextInstrument . '_complete'] != "" || $recordData[$record]['repeat_instances'][$event_id][$nextInstrument][$repeat_instance][$nextInstrument . '_complete'] != "") {
                         $surveyHashCode = $this->surveyHashByInstrument($project_id, $record, $nextInstrument, $event_id, $repeat_instance);
                         $redirectURL = APP_PATH_WEBROOT_FULL . "surveys/?s=" . $surveyHashCode["hash"];
